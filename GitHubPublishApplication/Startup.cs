621301fb13c858db3cb5c47d9d7dfd75959abc2e
gauravanandwhiz <gauravanandwhiz@gmail.com>
2015-06-15 17:21:06 +0530

    Adding MVC

---
+++ GitHubPublishApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHubPublishApplication.Startup))]
+namespace GitHubPublishApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
